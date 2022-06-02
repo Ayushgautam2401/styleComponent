ed8489813516627996db0d48c72e7552cbfba53b
ashu78890 <ashish.choudhary@biztecno.net>
2022-06-02 11:33:25 +0530

    styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { createGlobalStyle} from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+ const GlobalStyle = createGlobalStyle`
+ html,body{
+   font-size:16px;
+   margin:0;
+   padding:0;
+   font-family: 'Courier New', Courier, monospace;
 
+ }`
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
+    </GlobalStyle>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,36 @@
+export const theme ={
+    Text:{
+        default:'black',
+        primary:'grey',
+        secondary:'green'
+    },
+    background:{
+        main:'#7ABD43',
+        primary:'#FFFF00',
+    },
+    size:{
+        xs:'0.4rem',
+        sm:'0.3rem',
+        md:'0.5rem',
+        lg:'1rem',
+        exlg:'1.25rem',
+        ex2lg:'1.5rem',
+        ex3lg:'1.75rem',
+        ex4lg:'2rem'
+    },
+    color:{
+        primary:'red',
+        secondary:'blue',
+        defaultL:'green'
+    },
+    bordersize:{
+        xs:'0.4rem',
+        sm:'0.3rem',
+        md:'0.5rem',
+        lg:'1rem',
+        exlg:'1.25rem',
+        ex2lg:'1.5rem',
+        ex3lg:'1.75rem',
+        ex4lg:'2rem'
+    }
+}
