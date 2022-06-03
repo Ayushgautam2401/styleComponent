ea1dbf018ab356fa43997fa90471b1a141960ee0
ramanpreetkaur19 <ramanpreet.kaur@biztecno.net>
2022-06-03 16:11:27 +0530

    Layout Updated

--- src/Container/layout/LayoutStyling.js
+++ src/Container/layout/LayoutStyling.js
@@ -26,6 +26,7 @@ export const DisplayArea = styled.div`
     display: flex;
     flex-direction: row;
     height: calc(100% - 120px);
+    
 `
 export const Sidebar = styled.div
   .attrs(props => {
@@ -44,10 +45,55 @@ export const Sidebar = styled.div
 
 export const ContentArea = styled.div`
     display: flex;
-    height: 100%;
+    flex-direction: column;
+    height:100% ;
     width: 85%;
     overflow: auto;
+
 `
+export const TitleBar = styled.div `
+    display: flex;
+    flex-direction: row;
+    //position:sticky;
+    width: 100%;
+    height: 50px;
+    background-color: beige;
+    border-radius: 0.8rem;
+   // border: 1px solid black;
+
+`
+export const TitleLabel = styled.text
+.attrs(props => {
+  const { theme: { Text: { secondary, ...resttc }, size:{ex2lg, ...restts} } } = props;
+  return {
+    tc: resttc[props.tc] || secondary,
+    ts: restts[props.ts] || ex2lg,
+  }
+})
+`
+color:${props => props.tc};
+font-size:${props => props.ts};
+width: 20%;
+float: left;
+padding-left:1rem;
+padding-top:0.5rem;
+font-weight:bold;
+
+
+    
+
+
+`
+export const Content = styled.div`
+   display: flex;
+   width: 100%;
+   height:calc(100% - 50px);
+  // border: 2px solid blue;
+   overflow: auto;
+
+
+`
+
 
 export const Footer = styled.footer
   .attrs(props => {
