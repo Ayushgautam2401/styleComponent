7eabe156da01ffb5b91e0942d72b9a7ee139844a
Ayushgautam2401 <ayush.gautam@biztecno.net>
2022-06-03 19:25:50 +0530

    conflict resolve

--- src/Container/layout/LayoutStyling.js
+++ src/Container/layout/LayoutStyling.js
@@ -53,46 +53,6 @@ export const ContentArea = styled.div`
     overflow: auto;
 
 `
-export const TitleBar = styled.div `
-    display: flex;
-    flex-direction: row;
-    //position:sticky;
-    width: 100%;
-    height: 50px;
-    background-color: beige;
-    border-radius: 0.8rem;
-   // border: 1px solid black;
-
-`
-export const TitleLabel = styled.text
-.attrs(props => {
-  const { theme: { Text: { secondary, ...resttc }, size:{ex2lg, ...restts} } } = props;
-  return {
-    tc: resttc[props.tc] || secondary,
-    ts: restts[props.ts] || ex2lg,
-  }
-})
-`
-color:${props => props.tc};
-font-size:${props => props.ts};
-width: 20%;
-float: left;
-padding-left:1rem;
-padding-top:0.5rem;
-font-weight:bold;
-
-`
-export const Content = styled.div`
-   display: flex;
-   width: 100%;
-   height:calc(100% - 50px);
-  // border: 2px solid blue;
-   overflow: auto;
-
-
-`
-
-
 export const Footer = styled.footer
   .attrs(props => {
     const { theme: { background } } = props;
