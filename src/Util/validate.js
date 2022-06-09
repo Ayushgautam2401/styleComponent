4e09581db22a0b92bda9b5fdda8a289012bc42b8
Ayushgautam2401 <ayush.gautam@biztecno.net>
2022-06-09 16:58:45 +0530

    finalCommit

--- src/Util/validate.js
+++ src/Util/validate.js
@@ -19,39 +19,11 @@ export const email = value =>
 
 
 
-
-// export const validate = values => {
-//   const errors = {}
-  // if (!values.username) {
-  //   errors.username = 'Required'
-  // } else if (values.username.length > 15) {
-  //   errors.username = 'Must be 15 characters or less'
-  // }
-  // if (!values.email || values.email.length === 0) {
-  //   errors.email = 'Required'
-  // }
-  // else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-  //   errors.email = 'Invalid email address'
-  // }
-
-  // if (!values.age) {
-  //   errors.age = 'Required'
-  // } else if (isNaN(Number(values.age))) {
-  //   errors.age = 'Must be a number'
-  // } else if (Number(values.age) < 18) {
-  //   errors.age = 'Sorry, you must be at least 18 years old'
-  // }
- // return errors
-// }
-
-
-
 export const RenderField = ({
   input,
   label,
   type,
-  meta: { touched, error, warning
-  }
+  meta: { touched, error, warning}
 }) => (
   <div>
     {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
