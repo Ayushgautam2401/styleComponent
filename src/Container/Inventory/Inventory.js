647aa30b1255d1fa17ba61e14eea47117b3c4e3e
ramanpreetkaur19 <ramanpreet.kaur@biztecno.net>
2022-06-24 18:25:45 +0530

    inventory saga added

--- src/Container/Inventory/Inventory.js
+++ src/Container/Inventory/Inventory.js
@@ -1,15 +1,19 @@
 import { Styledbutton } from 'Components/Inputs/button'
 import React from 'react'
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom'
+import { getInventoryState } from 'Store/Selector';
 
 function Inventory() {
     const history = useHistory();
-    
+    const {list, raw} = useSelector(getInventoryState);
+
+
 
 
   return (
       <div>Inventory
-    <Styledbutton onClick={() => history.push("/inventory/Inventoryform")}>Add Category</Styledbutton>
+    <Styledbutton onClick={() => history.push("/inventory/Inventoryform")}>Add Inventory</Styledbutton>
     </div>
   )
 }
